6e1598a9e12f2ccafd2d8e3c1c6433f2ab1b3b6d
bartoszstefaniak88 <bartoszstefaniak88@users.norepley.github.com>
2018-12-09 10:11:17 +0100

    pierwszy program w pythonie

---
+++ pierwszy.py
@@ -0,0 +1,7 @@
+samochod = 'Honda'
+model = 'civic'
+spalanie = 10
+cena = 99999
+print ("Nazwa samochodu: " + samochod + model)
+print ("spalanie: " + str(spalanie))
+print ("cena samochodu:" + str(cena))
