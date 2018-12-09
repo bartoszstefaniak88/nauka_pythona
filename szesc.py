9cbb32d2d61ad9f8ec0565603d73e7e11f913f63
bartoszstefaniak88 <bartoszstefaniak88@users.norepley.github.com>
2018-12-09 11:47:50 +0100

    kolejne cwiczenie 6

---
+++ szesc.py
@@ -0,0 +1,7 @@
+owoce = ['jablko', 'gruszka','banan', 'arbuz']
+
+owoece.append('pomarancz')
+ost = samochody [-1]
+sublisty = samochody [1:3]
+bezpierwszego = samochody [1:]
+bezpierwszego i ostatniego = samochody [1:-1]
