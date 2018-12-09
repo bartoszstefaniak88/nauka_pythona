bff26a09bf29ab5f9db2718105b6a15519252c63
bartoszstefaniak88 <bartoszstefaniak88@users.norepley.github.com>
2018-12-09 11:19:01 +0100

    czarty program promocja

---
+++ czwarty.py
@@ -0,0 +1,14 @@
+cena = 100
+produkt = 'mleko'
+suma = 150
+
+if 100 < suma and suma <200:
+    print('Promocja 30%!')
+    suma = suma - (suma * 30) / 100
+elif suma >=200:
+    print ('Promocja 35%')
+    suma = suma - (suma * 35) / 100
+else:
+    print('bez promocji!')
+
+print('Do zaplacenia' + str(suma))
